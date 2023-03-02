211c1bc425b4889a45edbeb780ff147233ee73c8
agusbenit <126735556+agusbenit@users.noreply.github.com>
2023-03-01 23:04:14 -0300

    Add files via upload

---
+++ JS/function.js
@@ -0,0 +1,4 @@
+document.getElementById('boton').onclick = function () {
+    console.log("Capturamos el evento click")
+    document.getElementById("demo").innerHTML = "Universidad de moron: competencias en word, excel y windows nivel 3"
+}
